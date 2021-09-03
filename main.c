9d5cc5030132a1876384d036f94581cbb2de2685
HetDaftary <hetdaftary@gmail.com>
2021-09-03 10:33:17 +0530

    Adding fact numbers

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-unsigned long long int fibo(int a) {
+unsigned long long int fibo(unsigned int a) {
     if (a == 0) {
         return 0;
     } else if (a == 1) {
@@ -17,12 +17,27 @@ unsigned long long int fibo(int a) {
 
     return ans;
 }
+/**
+ * @brief Takes out factorial of a number
+ *  n! = n * (n - 1) * (n - 2) * ... * 1
+ * n! = n * (n - 1)!
+ * (n - 1)! = (n - 1) * (n - 2) * ... * 1
+ * 0! = 1
+ */
+unsigned long long int fact(unsigned int a) {
+    if (a < 2) {
+        return 1;
+    } 
+
+    return fact(a - 1) * a;
+}
 
 int main() {
     printf("Hello World!\n");
     printf("Hello world again\n");
 
-    printf("Fibo of %d is %llu", 3, fibo(3));
+    printf("Fibo of %u is %llu", 3, fibo(3));
+    printf("Fact of %u is %llu", 5, fact(5));
 
     return 0;
 }
